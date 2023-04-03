e3b7b2893ccda3e6baf377e9cb4d2baf46192af5
antala1012 <antala1012@gmail.com>
2023-04-03 18:01:54 +0530

    some Changes

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb://127.0.0.1:27017/mern`, () => {
+mongoose.connect('mongodb+srv://antala1012:euYWxSUGwT4K2BII@cluster0.al3px5g.mongodb.net/?retryWrites=true&w=majority', () => {
   console.log('connected to mongodb')
 })
